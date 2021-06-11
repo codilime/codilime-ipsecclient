89d22575a9383094c0c180aecfaad039a03a4ba8
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-11 09:19:30 +0200

    build react component for application containment

--- ipsec-ui-react/src/index.js
+++ ipsec-ui-react/src/index.js
@@ -1,1 +1,14 @@
-console.log("this is a test message from the system");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const title = 'Mockup of IPSec';
+
+ReactDOM.render(
+    <App title={title} />
+    <div>{title}</div>,
+    document.getElementById('app')
+);
+
+module.hot.accept();
