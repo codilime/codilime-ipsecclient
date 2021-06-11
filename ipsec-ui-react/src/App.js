420e6c265785b6ef835b78be8e8a069352a75a8c
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-11 10:52:19 +0200

    update structure for css loaders, needs troubleshooting

--- ipsec-ui-react/src/App.js
+++ ipsec-ui-react/src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+//import './App.css';
 
-const App = ({ title }) =>
-    <div>{title}</div>;
-
-export default App;
+export default function App(props) {
+    return (
+        <div className="app">
+            <div className="app-container">
+                <div className="top-bar">
+                    this is the place for logo
+                </div>
+                <div className="VRF-selection-panel">
+                    this is a placeholder for VRFs list component and adding a new one
+                </div>
+                <div className="VRF-settings-window">
+                    this is a placeholder for VRF setup
+                </div>
+            </div>
+        </div>
+    )
+}
--- ipsec-ui-react/src/index.js
+++ ipsec-ui-react/src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-const title = 'Mockup of IPSec';
-
 ReactDOM.render(
-    <App title={title} />
-    <div>{title}</div>,
+    <App />,
     document.getElementById('app')
 );
 
