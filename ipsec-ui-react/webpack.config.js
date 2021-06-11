a7f5b30a7f17a3146f82786ffba02048ba073a3f
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-11 10:52:19 +0200

    update structure for css loaders, needs troubleshooting

--- ipsec-ui-react/webpack.config.js
+++ ipsec-ui-react/webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
+                use: ['babel-loader'],
+            },
+        ],
     },
     resolve: {
         extensions: ['*', '.js', '.jsx'],
