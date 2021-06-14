f31f910ff7b977ad7c0f90a2bdd9085ed2f135e2
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-14 16:42:11 +0200

    implement testing solution from react router

--- ipsec-ui-react/src/SideBarComponent.js
+++ ipsec-ui-react/src/SideBarComponent.js
@@ -4,61 +4,79 @@ import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 import ciscoLogo from './images/cisco_logo.png';
 
+const routes = [
+    {
+        path: "/",
+        exact: true,
+        sidebar: () => <div>home!</div>,
+        main: () => <h2>Home</h2>
+    },
+    {
+        path: "/bubblegum",
+        sidebar: () => <div>bubblegum!</div>,
+        main: () => <h2>Bubblegum</h2>
+    },
+    {
+        path: "/shoelaces",
+        sidebar: () => <div>shoelaces!</div>,
+        main: () => <h2>Shoelaces</h2>
+    }
+];
+
 export default function SideBarComponent(props) {
     return (
         <div className="sidebar-container">
             <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Dashboard</Link>
-                        </li>
-                    </ul>
+                <div style={{ display: "flex" }}>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/bubblegum">Bubblegum</Link>
+                            </li>
+                            <li>
+                                <Link to="/shoelaces">Shoelaces</Link>
+                            </li>
+                        </ul>
 
-                    <hr />
-                    <Switch>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="/dashboard">
-                            <Dashboard />
-                        </Route>
-                    </Switch>
+                        <Switch>
+                            {routes.map((route, index) => (
+                                // You can render a <Route> in as many places
+                                // as you want in your app. It will render along
+                                // with any other <Route>s that also match the URL.
+                                // So, a sidebar or breadcrumbs or anything else
+                                // that requires you to render multiple things
+                                // in multiple places at the same URL is nothing
+                                // more than multiple <Route>s.
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    children={<route.sidebar />}
+                                />
+                            ))}
+                        </Switch>
+                    </div>
+
+                    <div style={{ flex: 1, padding: "10px" }}>
+                        <Switch>
+                            {routes.map((route, index) => (
+                                // Render more <Route>s with the same paths as
+                                // above, but different components this time.
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    exact={route.exact}
+                                    children={<route.main />}
+                                />
+                            ))}
+                        </Switch>
+                    </div>
                 </div>
             </Router>
         </div>
     );
 }
 
-function Home() {
-    return (
-        <div className="vrf-settings">
-            <h2>Home</h2>
-        </div>
-    );
-}
-
-function About() {
-    return (
-        <div className="vrf-settings">
-            <h2>About</h2>
-        </div>
-    );
-}
-
-function Dashboard() {
-    return (
-        <div className="vrf-settings">
-            <h2>Dashboard</h2>
-        </div>
-    );
-}
