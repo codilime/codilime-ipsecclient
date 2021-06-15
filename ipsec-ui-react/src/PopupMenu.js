81c2864d97e51113b62f18e1527a9348c820420c
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-15 10:21:38 +0200

    cleanup; add styling for the popupmenu component

---
+++ ipsec-ui-react/src/PopupMenu.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+class PopupMenu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showMenu: false,
+        };
+
+        this.showMenu = this.showMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+    }
+
+    showMenu(event) {
+        event.preventDefault();
+
+        this.setState({ showMenu: true }, () => {
+            document.addEventListener('click', this.closeMenu, {capture: true});
+        });
+    }
+
+    closeMenu(event) {
+        if (this.dropdownMenu && !this.dropdownMenu.contains(event.target)) {
+            this.setState({ showMenu: false }, () => {
+                document.removeEventListener('click', this.closeMenu);
+            });
+        }
+    }
+
+    render() {
+        let menu = (
+            <ul className="popupContents" ref={(element) => { this.dropdownMenu = element; }}>
+                {this.props.children}
+            </ul>
+        );
+
+        return (
+            <span className="popupMenu" {...this.props}>
+				<i className="popupTrigger fas fa-ellipsis-v" onClick={this.showMenu}/>
+                {this.state.showMenu ? menu : null}
+			</span>
+        );
+    }
+}
+
+export default PopupMenu
