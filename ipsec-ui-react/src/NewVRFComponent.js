c2491e15e444a8ad73d1a4099319eed3ceb36d2a
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-16 10:33:33 +0200

    add guidelines-compliant favicon.png to project; adjust styles for SideBarComponent.scss

--- ipsec-ui-react/src/NewVRFComponent.js
+++ ipsec-ui-react/src/NewVRFComponent.js
@@ -7,12 +7,15 @@ export default function ({routeProps}) {
     return (
         <div className="new-vrf-connection">
             {routeProps.location.pathname}
-                <tbody>
-                    <tr>
-                        <th>Name: </th>
-                        <th>Active []</th>
-                    </tr>
-                </tbody>
+            <div className="new-vrf-details">
+                placeholder for name, vlan, crypto phase 1 & 2, active checkbox and bgp
+            </div>
+            <div className="endpoints-details">
+                placeholder for table with endpoints
+            </div>
+            <div className="visualization">
+                placeholder for connections visualization
+            </div>
         </div>
     );
 }
