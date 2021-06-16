9a32f453857930dc0e417d113c46130a2b49b609
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-16 15:22:40 +0200

    finish mockup for vrf settings part of new vrf

--- ipsec-ui-react/src/NewVRFComponent.js
+++ ipsec-ui-react/src/NewVRFComponent.js
@@ -4,13 +4,19 @@ import './NewVRFComponent.scss'
 export default function ({routeProps}) {
     console.log({routeProps});
 
+    function logClick () {
+        console.log("save button for vrf goes brrrrrr");
+    }
+
     return (
         <div className="vrf-connection-wrapper">
             {routeProps.location.pathname}
             <div className="new-vrf-details">
                 <table id="new-vrf-details-table">
                     <tr>
-                        VRF details
+                        <th colSpan="3">
+                            VRF details
+                        </th>
                     </tr>
                     <tr>
                         <td>
@@ -34,10 +40,15 @@ export default function ({routeProps}) {
                             Crypto phase 2:
                         </td>
                     </tr>
+                    <tr>
+                        <td colSpan="3" className="save-button-container">
+                            <button className="btn save-vrf-settings-btn" onClick={logClick}>
+                                Save changes
+                            </button>
+                        </td>
+                    </tr>
                 </table>
-                <button className="btn save-vrf-settings-btn">
-                    Save changes
-                </button>
+
             </div>
             <div className="endpoints-details">
                 placeholder for table with endpoints
