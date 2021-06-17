e4179f689e7cfad04cd467cf8571321f3bd51fc5
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-17 11:05:34 +0200

    test commit; troubleshooting webstorm not adding SideBar

--- ipsec-ui-react/src/SideBarComponent.js
+++ ipsec-ui-react/src/SideBarComponent.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import './SideBarComponent.scss';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import NewVRFComponent from './NewVRFComponent.js';
+import {HashRouter as Router, Switch, Route, Link, useParams} from 'react-router-dom';
 
 import ciscoLogo from './images/cisco_logo.png';
 
-const routes = [
-    {
-        path: "/",
-        exact: true,
-        sidebar: () => <div>home!</div>,
-        main: () => <h2>Home</h2>
-    },
-    {
-        path: "/bubblegum",
-        sidebar: () => <div>bubblegum!</div>,
-        main: () => <h2>Bubblegum</h2>
-    },
-    {
-        path: "/shoelaces",
-        sidebar: () => <div>shoelaces!</div>,
-        main: () => <h2>Shoelaces</h2>
-    }
-];
-
 export default function SideBarComponent({routeProps}) {
     return (
-        <div className="sidebar-container">
-            <Router>
-                <div style={{ display: "flex" }}>
+        <Router>
+            <div className="app-wrapper">
+                <div className="sidebar-container">
                     <div>
                         <ul>
                             <li>
@@ -51,43 +33,18 @@ export default function SideBarComponent({routeProps}) {
                                 </Link>
                             </li>
                         </ul>
-
-                        <Switch>
-                            {routes.map((route, index) => (
-                                // You can render a <Route> in as many places
-                                // as you want in your app. It will render along
-                                // with any other <Route>s that also match the URL.
-                                // So, a sidebar or breadcrumbs or anything else
-                                // that requires you to render multiple things
-                                // in multiple places at the same URL is nothing
-                                // more than multiple <Route>s.
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    exact={route.exact}
-                                    children={<route.sidebar />}
-                                />
-                            ))}
-                        </Switch>
-                    </div>
-
-                    <div style={{ flex: 1, padding: "10px" }}>
-                        <Switch>
-                            {routes.map((route, index) => (
-                                // Render more <Route>s with the same paths as
-                                // above, but different components this time.
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    exact={route.exact}
-                                    children={<route.main />}
-                                />
-                            ))}
-                        </Switch>
                     </div>
                 </div>
-            </Router>
-        </div>
+
+                <div className="vrf-details">
+                    <Switch>
+                        <Route path="/VRF/CREATE" render={routeProps => <div><NewVRFComponent routeProps={routeProps} /></div>} />
+                        <Route path="/vrf/:id" render={routeProps => <div style={{ display: "flex" }}>/VRFs/{routeProps.match.params.id}</div>} />
+                        <Route path="*" render={() => 404} />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
     );
 }
 
