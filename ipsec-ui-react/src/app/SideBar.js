80b6797b7c9e4bb068d1cfbe8dab0aa6871b68e5
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-21 14:59:57 +0200

    refactor of SideBar.scss

--- ipsec-ui-react/src/app/SideBar.js
+++ ipsec-ui-react/src/app/SideBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './SideBar.scss';
-import NewVRF from '../pages/NewVRF.js';
 import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 
@@ -11,43 +10,33 @@ export default function SideBar() {
     }
     return (
         <Router>
-            <div className="app-wrapper">
-                <div className="sidebar-container">
-                    <div>
-                        <ul>
-                            <li onClick={clickTester}>
-                                <Link to="/vrf/V101:U">V101:U</Link>
-                            </li>
-                            <li onClick={clickTester}>
-                                <Link to="/vrf/V102:SoccerOnline">V102:SoccerOnline</Link>
-                            </li>
-                            <li onClick={clickTester}>
-                                <Link to="/vrf/V103:BigBank">V103:BigBank</Link>
-                            </li>
-                            <li onClick={clickTester}>
-                                <Link to="/vrf/V104:BranchOffice">V104:BranchOffice</Link>
-                            </li>
-                            <li onClick={clickTester}>
-                                <Link to="/vrf/V105:test">V105:test</Link>
-                            </li>
-                            <li onClick={clickTester}>
-                                <Link to="/VRF/CREATE">
-                                    <button className="btn new-vrf-button">Add a new VRF</button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div className="vrf-details">
-                    <Switch>
-                        <Route path="/VRF/CREATE" render={routeProps => <div><NewVRF routeProps={routeProps} /></div>} />
-                        <Route path="/vrf/:id" render={routeProps => <div style={{ display: "flex" }}>/VRFs/{routeProps.match.params.id}</div>} />
-                        <Route path="*" render={() => 404} />
-                    </Switch>
+            <div className="sidebar-container">
+                <div>
+                    <ul>
+                        <li onClick={clickTester}>
+                            <Link to="/vrf/V101:U">V101:U</Link>
+                        </li>
+                        <li onClick={clickTester}>
+                            <Link to="/vrf/V102:SoccerOnline">V102:SoccerOnline</Link>
+                        </li>
+                        <li onClick={clickTester}>
+                            <Link to="/vrf/V103:BigBank">V103:BigBank</Link>
+                        </li>
+                        <li onClick={clickTester}>
+                            <Link to="/vrf/V104:BranchOffice">V104:BranchOffice</Link>
+                        </li>
+                        <li onClick={clickTester}>
+                            <Link to="/vrf/V105:test">V105:test</Link>
+                        </li>
+                        <li onClick={clickTester}>
+                            <Link to="/VRF/CREATE">
+                                <button className="btn new-vrf-button">Add a new VRF</button>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </Router>
+     </Router>
     );
 }
 
