95f436b11ce99852301eb444e7de5978fa7800c4
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-21 15:26:45 +0200

    add basic styles for mainview component

--- ipsec-ui-react/src/app/App.js
+++ ipsec-ui-react/src/app/App.js
@@ -8,7 +8,7 @@ import './App.scss';
 export default function App() {
 
     return (
-        <div className="app">
+        <div className="app-container">
             <TopBar />
             <Sidebar />
             <MainView />
