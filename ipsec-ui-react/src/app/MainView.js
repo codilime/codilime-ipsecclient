f82498b75b06ce100f800b538705c4117f8b5207
Piotr Slezak <piotr.slezak@codilime.com>
2021-06-21 15:40:20 +0200

    implement cross-component Router connection

--- ipsec-ui-react/src/app/MainView.js
+++ ipsec-ui-react/src/app/MainView.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './MainView.scss';
+import NewVRF from "../pages/NewVRF";
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 
 export default function MainView () {
     return (
         <div className="main-view-container">
             <div className="vrf-detail-view">
-                this is box for particular box
+                <Router>
+                        <Switch>
+                            <Route path="/VRF/CREATE" render={routeProps => <div><NewVRF routeProps={routeProps} /></div>} />
+                            <Route path="/vrf/:id" render={routeProps => <div style={{ display: "flex" }}>/VRFs/{routeProps.match.params.id}</div>} />
+                            <Route path="*" render={() => 404} />
+                        </Switch>
+                </Router>
             </div>
         </div>
     )
