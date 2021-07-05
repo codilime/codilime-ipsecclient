754eb23f8a2af8e1ebcae133db20fe3cff7129c5
Piotr Slezak <piotr.slezak@codilime.com>
2021-07-05 13:28:01 +0200

    add save button for submitting data do API

--- ipsec-ui-react/src/pages/DetailVRF.js
+++ ipsec-ui-react/src/pages/DetailVRF.js
@@ -5,7 +5,7 @@ import {v4 as uuidv4} from 'uuid';
 import Dump from "../components/Dump";
 
 export default function detailViewVrf(props) {
-    const detailVRF = props.VRFdata;
+    const detailVrf = props.VRFdata;
 
     if(detailVRF == null){
         return(
@@ -16,7 +16,7 @@ export default function detailViewVrf(props) {
     }
     return(
         <div className="vrf-detail-container">
-            /vrf/{detailVRF.client_name}<br />
+            /vrf/{detailVrf.client_name}<br />
             <div className="vrf-detail-section-container">
                 <div className="vrf-details-bar">VRF Details</div>
                 <form>
@@ -25,7 +25,7 @@ export default function detailViewVrf(props) {
                     <div className="vrf-column-1">
                         <div className="vrf-column-item">
                             <label htmlFor="client_name">Name:</label>
-                            <input type="text" name="client_name" id="client_name" readOnly={detailVRF.client_name} value={detailVRF.client_name}/>
+                            <input type="text" name="client_name" id="client_name" readOnly={detailVrf.client_name} value={detailVrf.client_name}/>
                         </div>
                         <div className="vrf-column-item">
                             <input type="checkbox" name="active" id="active"/>
@@ -33,63 +33,32 @@ export default function detailViewVrf(props) {
                         </div>
                     </div>
                     <div className="vrf-column-2">
-                        <div className="vrf-column-item">
+                        <div className="vrf-column-item-number">
                             <label htmlFor="vlan">VLAN</label>
-                            <input type="number" name="vlan" id="vlan" step="1" readOnly={detailVRF.vlan} value={detailVRF.vlan}/>
+                            <input type="number" name="vlan" id="vlan" step="1" readOnly={detailVrf.vlan} value={detailVrf.vlan}/>
                         </div>
-                        <div className="vrf-column-item">
+                        <div className="vrf-column-item-number">
                             <label htmlFor="local_as">BGP local AS</label>
-                            <input type="number" name="local_as" id="local_as" step="1" readOnly={detailVRF.local_as} value={detailVRF.local_as}/>
+                            <input type="number" name="local_as" id="local_as" step="1"
+                                   readOnly={detailVrf.local_as}
+                                   value={detailVrf.local_as}/>
                         </div>
                     </div>
                     <div className="vrf-column-3">
                         <div className="vrf-crypto-container">
                             <label htmlFor="crypto_ph1">Crypto phase 1</label>
-                            <input type="text" id="crypto_ph1_1" name="crypto_ph1" readOnly={detailVRF.crypto_ph1[0]} value={detailVRF.crypto_ph1[0]} />
-                            <input type="text" id="crypto_ph1_2" name="crypto_ph1" readOnly={detailVRF.crypto_ph1[1]} value={detailVRF.crypto_ph1[1]} />
-                            <input type="text" id="crypto_ph1_3" name="crypto_ph1" readOnly={detailVRF.crypto_ph1[2]} value={detailVRF.crypto_ph1[2]} />
+                            <input type="text" id="crypto_ph1_1" name="crypto_ph1" readOnly={detailVrf.crypto_ph1[0]} value={detailVrf.crypto_ph1[0]} />
+                            <input type="text" id="crypto_ph1_2" name="crypto_ph1" readOnly={detailVrf.crypto_ph1[1]} value={detailVrf.crypto_ph1[1]} />
+                            <input type="text" id="crypto_ph1_3" name="crypto_ph1" readOnly={detailVrf.crypto_ph1[2]} value={detailVrf.crypto_ph1[2]} />
                         </div>
                         <div className="vrf-crypto-container">
                             <label htmlFor="crypto_ph2">Crypto phase 2</label>
-                            <input type="text" id="crypto_ph2_1" name="crypto_ph2" readOnly={detailVRF.crypto_ph2[0]} value={detailVRF.crypto_ph2[0]} />
-                            <input type="text" id="crypto_ph2_2" name="crypto_ph2" readOnly={detailVRF.crypto_ph2[1]} value={detailVRF.crypto_ph2[1]} />
-                            <input type="text" id="crypto_ph2_3" name="crypto_ph2" readOnly={detailVRF.crypto_ph2[2]} value={detailVRF.crypto_ph2[2]} />
+                            <input type="text" id="crypto_ph2_1" name="crypto_ph2" readOnly={detailVrf.crypto_ph2[0]} value={detailVrf.crypto_ph2[0]} />
+                            <input type="text" id="crypto_ph2_2" name="crypto_ph2" readOnly={detailVrf.crypto_ph2[1]} value={detailVrf.crypto_ph2[1]} />
+                            <input type="text" id="crypto_ph2_3" name="crypto_ph2" readOnly={detailVrf.crypto_ph2[2]} value={detailVrf.crypto_ph2[2]} />
                         </div>
                     </div>
                 </form>
-                {/*<table>*/}
-                {/*    <thead>*/}
-                {/*        <tr>*/}
-                {/*            <th>*/}
-                {/*                VRF Details*/}
-                {/*            </th>*/}
-                {/*        </tr>*/}
-                {/*    </thead>*/}
-                {/*    <tbody>*/}
-                {/*        <tr className="vrf-main-settings-table">*/}
-                {/*            <td>Name:</td>*/}
-                {/*            <td className="break">{detailVRF.client_name}</td>*/}
-                {/*            <td>VLAN:</td>*/}
-                {/*            <td className="break">{detailVRF.vlan}</td>*/}
-                {/*            <td>Crypto phase 1:</td>*/}
-                {/*            <td>{detailVRF.crypto_ph1[0]}</td>*/}
-                {/*            <td>{detailVRF.crypto_ph1[1]}</td>*/}
-                {/*            <td>{detailVRF.crypto_ph1[2]}</td>*/}
-                {/*        </tr>*/}
-                {/*        <tr>*/}
-                {/*            <td className="checkbox-holder">*/}
-                {/*                <input type="checkbox" id="active_checkbox"/>*/}
-                {/*            </td>*/}
-                {/*            <td className="break"><p>Active</p></td>*/}
-                {/*            <td>BGP local as:</td>*/}
-                {/*            <td className="break">{detailVRF.local_as}</td>*/}
-                {/*            <td >Crypto phase 2:</td>*/}
-                {/*            <td>{detailVRF.crypto_ph2[0]}</td>*/}
-                {/*            <td>{detailVRF.crypto_ph2[1]}</td>*/}
-                {/*            <td>{detailVRF.crypto_ph2[2]}</td>*/}
-                {/*        </tr>*/}
-                {/*    </tbody>*/}
-                {/*</table>*/}
             </div>
             <div className="vrf-detail-section-container">
                 <table id="endpoints-table">
@@ -110,7 +79,7 @@ export default function detailViewVrf(props) {
                             <th>BGP</th>
                             <th>Action</th>
                         </tr>
-                            {detailVRF && detailVRF.endpoints && detailVRF.endpoints.map(function(endpoint) {
+                            {detailVrf && detailVrf.endpoints && detailVrf.endpoints.map(function(endpoint) {
                                 return (
                                     <tr key={uuidv4()}>
                                         <td>{endpoint.remote_ip_sec}</td>
@@ -131,7 +100,7 @@ export default function detailViewVrf(props) {
             <div className="vrf-detail-section-container">
                 visu
             </div>
-            <Dump value={detailVRF} />
+            <Dump value={detailVrf} />
         </div>
     )
 }
--- ipsec-ui-react/src/pages/NewVRF.js
+++ ipsec-ui-react/src/pages/NewVRF.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewVRF.scss'
 import Dump from "../components/Dump";
 import {v4 as uuidv4} from "uuid";
 
 export default function NewVRF({routeProps, cryptoPhaseEncryption}) {
+    const [vrfName, updateVrfName] = useState("");
+    const [vlanValue, updateVlanValue] = useState("-1");
+    const [bgpValue, updateBgpValue] = useState("-1");
+    const [cryptoPh1, updateCryptoPh1] = useState([]);
+    const [cryptoPh2, updateCryptoPh2] = useState([])
+
+    const saveButton = document.getElementById("submit-vrf-button");
+    if(vrfName) {
+        saveButton.removeAttribute("disabled");
+    } else {
+        saveButton.setAttribute("disabled", "disabled");
+    }
+
+    function saveCryptoData(event) {
+        // event.preventDefault();
+        console.log("updating");
+        // updateCryptoPh1();
+        // updateCryptoPh2();
+    }
 
     return (
         <div className="new-vrf-connection-wrapper">
@@ -16,26 +35,29 @@ export default function NewVRF({routeProps, cryptoPhaseEncryption}) {
                     <div className="vrf-column-1">
                         <div className="vrf-column-item">
                             <label htmlFor="client_name">Name:</label>
-                            <input type="text" name="client_name" id="client_name"/>
+                            <input type="text" name="client_name" id="client_name" onChange={event => updateVrfName(event.target.value)}/>
                         </div>
                         <div className="vrf-column-item">
                             <input type="checkbox" name="active" id="active"/>
                             <label htmlFor="Active">Active</label>
                         </div>
+                        <button className="btn" id="submit-vrf-button" onClick={saveCryptoData} disabled>Save changes</button>
                     </div>
                     <div className="vrf-column-2">
-                        <div className="vrf-column-item">
+                        <div className="vrf-column-item-number">
                             <label htmlFor="vlan">VLAN</label>
-                            <input type="number" name="vlan" id="vlan" step="1"/>
+                            <input type="number" name="vlan" id="vlan" step="1" defaultValue={vlanValue}
+                                   onChange={event => updateVlanValue(event.target.value)}/>
                         </div>
-                        <div className="vrf-column-item">
+                        <div className="vrf-column-item-number">
                             <label htmlFor="local_as">BGP local AS</label>
-                            <input type="number" name="local_as" id="local_as" step="1"/>
+                            <input type="number" name="local_as" id="local_as" step="1" defaultValue={bgpValue}
+                                   onChange={event => updateBgpValue(event.target.value)}/>
                         </div>
                     </div>
                     <div className="vrf-column-3">
                         <div className="vrf-crypto-container">
-                           <label htmlFor="crypto_ph1">Crypto phase 1</label>
+                            <label htmlFor="crypto_ph1">Crypto phase 1</label>
                             <select id="crypto_ph1" name="crypto_ph1">
                                 {cryptoPhaseEncryption && cryptoPhaseEncryption.encryption
                                 && cryptoPhaseEncryption.encryption.map(function(element) {
