39be89a9a9f7c389b9dbc41a3723aa48ff480f92
Piotr Slezak <piotr.slezak@codilime.com>
2021-07-19 13:21:08 +0200

    implement Object.entries to map endpoint connections and values

--- ipsec-ui-react/src/components/EndpointTableCell.jsx
+++ ipsec-ui-react/src/components/EndpointTableCell.jsx
@@ -1,31 +1,10 @@
-import React, {useEffect} from "react";
-import Dump from "./Dump";
-import {renderToString} from "react-dom/server";
+import React from "react";
 
-export default function EndpointTableCell(thisEndpoint) {
-    const data = thisEndpoint.thisEndpoint;
+export default function EndpointTableCell(endpointTableValue) {
 
     return (
-        <>
-            <td>
-                <div>{data.remote_ip_sec}</div>
-            </td>
-            <td>
-                <div>{data.local_ip}</div>
-            </td>
-            <td>
-                <div>{data.peer_ip}</div>
-            </td>
-            <td>
-                <div>{data.psk}</div>
-            </td>
-            <td>
-                <div>nat value</div>
-            </td>
-            <td>
-                <div>bgp value</div>
-            </td>
-        </>
-
+        <td>
+            {endpointTableValue}
+        </td>
     )
 }
