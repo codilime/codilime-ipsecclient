942aa9f105ffd3d1d12697205631892c00bf565e
Piotr Slezak <piotr.slezak@codilime.com>
2021-07-24 16:56:04 +0200

    cleanup of Dump.jsx

--- ipsec-ui-react/src/components/Dump.jsx
+++ ipsec-ui-react/src/components/Dump.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 
 export default function Dump(props) {
-    let label = null;
-    if (props.label) {
-        label = (<span><strong>{props.label || ''}</strong> = </span>);
+
+    let {label, ...otherProps} = props;
+
+    if (label) {
+        label = (<span><strong>{label || ''}</strong> = </span>);
+    }
+
+    function isObject(obj) {
+        return obj === Object(obj);
+    }
+
+    function isFunction(functionToCheck) {
+        return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+    }
+
+    function prepareObject(object) {
+        let newObject = {};
+
+        Object.entries(object).map(([property, value]) => {
+            if (isFunction(value)) {
+                newObject[property] = 'function ' + value.name + '()';
+            } else {
+                newObject[property] = value;
+            }
+        });
+
+        return newObject;
+    }
+
+    function renderSingle(value) {
+        if (isObject(value)) {
+            value = prepareObject(value);
+        }
+
+        return JSON.stringify(prepareObject(value), null, 2);
+    }
+
+    function renderProps() {
+        let toReturn = [];
+
+        if(Array.isArray(otherProps)) {
+            otherProps.map(function(value) {
+                toReturn.push(renderSingle(otherProps));
+            });
+        } else {
+            toReturn = renderSingle(otherProps);
+        }
+
+        return toReturn;
     }
 
     return (
@@ -12,7 +58,7 @@ export default function Dump(props) {
             <small>
 				<pre style={{background: '#f0f3f5', padding: '.5rem'}}>
 					{label}
-                    {JSON.stringify(props.value, null, 2)}
+                    {renderProps()}
 	            </pre>
             </small>
         </div>
