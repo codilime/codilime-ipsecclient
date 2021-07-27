4ae6aef01cfa07373762746434018ba481ccbb40
Marcin Parafiniuk <marcin.parafiniuk@codilime.com>
2021-07-27 18:29:20 +0200

    wip

---
+++ run_net.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+docker rm sico_net || true 
+
+docker run --cap-add=NET_ADMIN \
+        --name sico_net \
+        --mount type=volume,source=ipsec,destination=/opt/ipsec/ \
+        --mount type=volume,source=bird,destination=/opt/bird/ \
+        --mount type=volume,source=super_net,destination=/opt/super_net/ \
+        --mount type=volume,source=super_api,destination=/opt/super_api/ \
+        sico_net
