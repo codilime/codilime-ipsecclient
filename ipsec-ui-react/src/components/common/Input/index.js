60782f79a4212f7c8fa4a878e4629f63561289b6
Piotr Slezak <piotr.slezak@codilime.com>
2021-08-05 12:57:33 +0200

    add styles for new field form

--- ipsec-ui-react/src/components/common/Input/index.js
+++ ipsec-ui-react/src/components/common/Input/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-// import 'style.scss';
+import './style.scss';
 
 export function Input({ type, name, placeholder, onChange, ref }) {
-  return <input type={type} name={name} placeholder={placeholder} onChange={onChange} ref={ref} />;
+  return <input className='field__input' type={type} name={name} placeholder={placeholder} onChange={onChange} ref={ref} />;
 }
 
 Input.propTypes = {
   type: PropTypes.string,
-  name: PropTypes.string.required,
+  name: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   onChange: PropTypes.func,
   ref: PropTypes.oneOfType([PropTypes.func, PropTypes.shape({ current: PropTypes.instanceOf(Element) })])
--- ipsec-ui-react/src/components/template/field/index.js
+++ ipsec-ui-react/src/components/template/field/index.js
@@ -3,12 +3,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Input } from "common";
+import './style.scss'
 
 
 export const Field = ({text, type, name, placeholder, onChange, ref}) => {
     return (
-        <div>
-            <label>{text}</label>
+        <div className='field'>
+            <label className='field__label'>{text}</label>
             <Input {...{type, name, placeholder, onChange, ref}}/>
         </div>
     )
@@ -16,7 +17,7 @@ export const Field = ({text, type, name, placeholder, onChange, ref}) => {
 
 Field.propTypes = {
     type: PropTypes.string,
-    name: PropTypes.string.required,
+    name: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
     onChange: PropTypes.func,
     text: PropTypes.string,
