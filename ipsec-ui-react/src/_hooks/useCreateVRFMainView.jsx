8718998bf6d63301f5790b4e381935156cd7ba4a
Piotr Slezak <piotr.slezak@codilime.com>
2021-08-06 11:44:30 +0200

    change useCreateVRFMainView.jsx to map elements by name instead of type

--- ipsec-ui-react/src/_hooks/useCreateVRFMainView.jsx
+++ ipsec-ui-react/src/_hooks/useCreateVRFMainView.jsx
@@ -6,12 +6,43 @@ import { Field } from '../components/template';
 export const useCreateVRFMainView = () => {
   const { mainVRFViewColumnOne, mainVRFViewColumnTwo, mainVRFViewColumnThree } = DynamicVRFView;
 
+  // const obj = {
+  //   "id": 1,
+  //   "client_name": "123",
+  //   "vlan": 1,
+  //   "crypto_ph1": [
+  //     "",
+  //     "",
+  //     ""
+  //   ],
+  //   "crypto_ph2": [
+  //     "",
+  //     "",
+  //     ""
+  //   ],
+  //   "physical_interface": "",
+  //   "active": false,
+  //   "hardware_support": false,
+  //   "local_as": 1,
+  //   "lan_ip": "asdasd",
+  //   "endpoints": null
+  // };
+
   const VRFColumnOneView = mainVRFViewColumnOne.map((el, index) => {
-    switch (el.type) {
-      case 'text': {
+    switch (el.name) {
+      case 'client_name': {
         return <Field {...el} />;
       }
-      case 'checkbox': {
+      case 'lan_ip': {
+        return <Field {...el} />;
+      }
+      case 'physical_interface': {
+        return <Field {...el} />;
+      }
+      case 'active': {
+        return <Field {...el} />;
+      }
+      case 'hardware_support': {
         return <Field {...el} />;
       }
       default:
@@ -19,8 +50,11 @@ export const useCreateVRFMainView = () => {
     }
   });
   const VRFColumnTwoView = mainVRFViewColumnTwo.map((el) => {
-    switch (el.type) {
-      case 'number': {
+    switch (el.name) {
+      case 'local_as': {
+        return <Field {...el} />;
+      }
+      case 'vlan': {
         return <Field {...el} />;
       }
       default:
@@ -28,8 +62,11 @@ export const useCreateVRFMainView = () => {
     }
   })
   const VRFColumnThreeView = mainVRFViewColumnThree.map((el) => {
-    switch (el.type) {
-      case 'array': {
+    switch (el.name) {
+      case 'crypto_ph1': {
+        return <Field {...el} />;
+      }
+      case 'crypto_ph2': {
         return <Field {...el} />;
       }
       default:
