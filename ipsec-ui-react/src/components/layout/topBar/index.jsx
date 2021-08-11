11c2fd1ff4d7def491a7f01abe556d1ab25aba28
Piotr Slezak <piotr.slezak@codilime.com>
2021-08-11 14:45:11 +0200

    correct display/CSS for topbar component

--- ipsec-ui-react/src/components/layout/topBar/index.jsx
+++ ipsec-ui-react/src/components/layout/topBar/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Logo from 'assets/cisco_logo.png';
 import PropTypes from 'prop-types';
 import './styles.scss';
+
 export const TopBar = ({ productName }) => (
   <header className="topBar">
     <img src={Logo} alt="cisco logo" className="topBar__image" />
     <p className="topBar__productName">{productName}</p>
   </header>
 );
+
 TopBar.defaultProps = {
   productName: 'IPsec User Interface'
 };
