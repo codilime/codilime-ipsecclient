1cf343cda9cbaf94af9d976d50696b1cd9357db6
AnGeL991 <adrian.markuszewski@codilime.com>
2021-08-12 14:07:39 +0200

    add classnames library

--- ipsec-ui-react/src/components/common/EndpointInput/index.jsx
+++ ipsec-ui-react/src/components/common/EndpointInput/index.jsx
@@ -1,58 +1,49 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { IoEyeSharp } from "react-icons/io5";
-import { BsEyeSlashFill } from "react-icons/bs";
-import { useToggle } from "hooks";
-import "./styles.scss";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { IoEyeSharp } from 'react-icons/io5';
+import { BsEyeSlashFill } from 'react-icons/bs';
+import { useToggle } from 'hooks';
+import classNames from 'classnames';
+import './styles.scss';
 
-export const EndpointInput = ({
-    type,
-    placeholder,
-    name,
-    value,
-    active,
-    onChange,
-    disabled,
-    checked,
-}) => {
-    const { open, handleToggle } = useToggle();
+export const EndpointInput = ({ type, placeholder, name, value, edit, onChange, disabled }) => {
+  const { open, handleToggle } = useToggle();
+  const [alert, setAlert] = useState(false);
 
-    const icon = open ? (
-        <BsEyeSlashFill
-            className="endpointInput__icon"
-            onClick={handleToggle}
-        />
-    ) : (
-        <IoEyeSharp className="endpointInput__icon" onClick={handleToggle} />
-    );
+  const icon = open ? <BsEyeSlashFill className="endpointInput__icon" onClick={handleToggle} /> : <IoEyeSharp className="endpointInput__icon" onClick={handleToggle} />;
 
-    const showEyes = type === "password" ? <>{icon}</> : null;
-
-    return (
-        <>
-            <input
-                className={`endpointInput 
-                ${type === "checkbox" ? "endpointInput__checkbox" : ""}
-                ${active ? "endpointInput__active" : ""}`}
-                type={open ? "text" : type}
-                {...{ name, placeholder, value, disabled, onChange }}
-            />
-            {showEyes}
-        </>
-    );
+  const showEyes = type === 'password' ? <>{icon}</> : null;
+  const validateDataInput = (event) => {
+    if (!/[0-9.]/.test(event.key) && event.target.name !== 'psk') {
+      event.preventDefault();
+      setAlert(true);
+      return;
+    }
+    setAlert(false);
+  };
+  return (
+    <>
+      <input
+        className={classNames({ endpointInput: true, endpointInput__checkbox: type === 'checkbox', endpointInput__active: edit })}
+        type={open ? 'text' : type}
+        onKeyPress={validateDataInput}
+        {...{ name, placeholder, value, disabled, onChange }}
+      />
+      {showEyes}
+    </>
+  );
 };
 
 EndpointInput.defaultProps = {
-    checked: true,
+  checked: true
 };
 
 EndpointInput.propTypes = {
-    type: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    placeholder: PropTypes.string,
-    checked: PropTypes.bool,
-    active: PropTypes.bool,
-    disabled: PropTypes.bool,
-    value: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
-    onChange: PropTypes.func,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  edit: PropTypes.bool,
+  disabled: PropTypes.bool,
+  value: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  onChange: PropTypes.func
 };
