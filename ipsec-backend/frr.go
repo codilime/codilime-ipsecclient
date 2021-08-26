37e5b2e88bb32a985023a682efa9cae4b93e7ddc
Marcin Parafiniuk <marcin.parafiniuk@codilime.com>
2021-08-26 14:55:00 +0200

    fixes

--- ipsec-backend/frr.go
+++ ipsec-backend/frr.go
@@ -21,14 +21,14 @@ func generateFRRTemplate(vrf Vrf) error {
 	if err := json.Unmarshal([]byte(vrf.Endpoints.String()), &endpoints); err != nil {
 		return err
 	}
-	createTmpFile := fmt.Sprintf("router bgp %d vrf vrf-%d\n", vrf.LocalAs, vrf.Vlan)
+	createTmpFile := fmt.Sprintf("router bgp %d vrf %s\n  no bgp ebgp-requires-policy", vrf.LocalAs, vrf.ClientName)
 	for _, endpoint := range endpoints {
-		createTmpFile += fmt.Sprintf("  neighbor %s remote-as external\n", endpoint.RemoteIPSec)
+		createTmpFile += fmt.Sprintf("  neighbor %s remote-as external\n", endpoint.PeerIP)
 	}
 	return runTmpVtyshFile(createTmpFile)
 }
 
 func deleteFRRTemplate(vrf Vrf) error {
-	deleteTmpFile := fmt.Sprintf("no router bgp %d vrf vrf-%d\n", vrf.LocalAs, vrf.Vlan)
+	deleteTmpFile := fmt.Sprintf("no router bgp %d vrf %s\n", vrf.LocalAs, vrf.ClientName)
 	return runTmpVtyshFile(deleteTmpFile)
 }
