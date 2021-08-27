3f930c58876fde907441bbb1e3079759a50bc593
AnGeL991 <adrian.markuszewski@codilime.com>
2021-08-27 11:53:39 +0200

    add icon on topBar

--- ipsec-ui-react/src/components/layout/topBar/index.jsx
+++ ipsec-ui-react/src/components/layout/topBar/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Logo from 'images/cisco_logo.png';
 import PropTypes from 'prop-types';
+import { TopSideIcon } from 'common';
+import { FiSettings, FiBell, FiLogOut } from 'react-icons/fi';
 import './styles.scss';
 
 export const TopBar = ({ productName }) => (
   <header className="topBar">
-    <img src={Logo} alt="cisco logo" className="topBar__image" />
-    <p className="topBar__productName">{productName}</p>
+    <div className="topBar__right">
+      <img src={Logo} alt="cisco logo" className="topBar__image" />
+      <p className="topBar__productName">{productName}</p>
+    </div>
+    <div className="topBar__left">
+      <TopSideIcon>
+        <FiBell className="topBar__icon" />
+      </TopSideIcon>
+      <TopSideIcon>
+        <FiSettings className="topBar__icon" />
+      </TopSideIcon>
+      <TopSideIcon>
+        <FiLogOut className="topBar__icon" />
+      </TopSideIcon>
+    </div>
   </header>
 );
 
