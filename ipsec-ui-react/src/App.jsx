8f4fc2247757c0f29c35b4cbc7b65545b9903ca5
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek-zdanowicz@codilime.com>
2021-08-31 11:05:09 +0200

    [IPSEC-SICO] HRM router

--- ipsec-ui-react/src/App.jsx
+++ ipsec-ui-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { MainLayout } from 'layout';
 import { Routers } from './routers';
 import 'style/global.scss';
