c2ea7da6ee70c8242dabb4330729986b5cd27391
Piotr Slezak <piotr.slezak@codilime.com>
2021-09-08 11:17:54 +0200

    refactor uploadCertificates to typescript types

---
+++ ipsec-ui-react/src/components/common/uploadCertificates/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import { EndpointInput, UploadButton } from 'common';
+import PropTypes from 'prop-types';
+
+interface IUploadCertificates {
+  type?: string;
+  name: string;
+  label?: string;
+  text?: string;
+  references?: any;
+  edit?: boolean;
+  onChange?: () => void;
+  onClick?: () => void;
+  value?: string;
+  className?: string;
+}
+
+export const UploadCertificates: FC<IUploadCertificates> = ({ type, name, onChange, edit, references, value, label, text, onClick, className }) => {
+  return (
+    <div className="table__certificates">
+      <EndpointInput {...{ type, name, onChange, edit, references, value }} />
+      {label && <p className="table__title">{label}</p>}
+      <UploadButton {...{ onClick, name, edit, className }}>{text}</UploadButton>
+    </div>
+  );
+};
