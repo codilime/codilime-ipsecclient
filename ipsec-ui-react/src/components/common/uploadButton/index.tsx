c389e99b66444dd99edd275924075f19c94057c3
Piotr Slezak <piotr.slezak@codilime.com>
2021-09-13 10:13:54 +0200

    refactor uploadButton and uploadCertificates to work properly

--- ipsec-ui-react/src/components/common/uploadButton/index.tsx
+++ ipsec-ui-react/src/components/common/uploadButton/index.tsx
@@ -4,13 +4,12 @@ import './styles.scss';
 import { ButtonTypeProps } from '../../../interface/components';
 
 interface UploadButtonProps extends ButtonTypeProps {
-  className: string;
   edit: boolean;
 }
 
 export const UploadButton: FC<UploadButtonProps> = ({ onClick, children, name, edit, className }) => {
   return (
-    <button {...{ onClick, name, disabled: !edit }} className={classNames('uploadButton', { uploadButton__disabled: !edit, [className]: className })}>
+    <button {...{ onClick, name, disabled: !edit }} className={classNames('uploadButton', { uploadButton__disabled: !edit, [className as any]: className })}>
       {children}
     </button>
   );
