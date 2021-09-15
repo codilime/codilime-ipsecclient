00dcf7e3dae10ddc37234dc9f3f7a00ce8a6d23f
AnGeL991 <adrian.markuszewski@codilime.com>
2021-09-15 12:26:33 +0200

    add save value to file

--- ipsec-ui-react/src/components/template/popup/popupLogs/index.tsx
+++ ipsec-ui-react/src/components/template/popup/popupLogs/index.tsx
@@ -11,11 +11,22 @@ interface PopupLogsType {
 export const PopupLogs: FC<PopupLogsType> = ({ logs = [], open, handleToggle }) => {
   const displayLogs = logs.map((log) => log);
 
+  const HandleDownloadTextFile = () => {
+    const element = document.createElement('a');
+    const file = new Blob(['hello test pliku tekstowego'], { type: 'text/plain;charset=utf-8' });
+    element.href = URL.createObjectURL(file);
+    element.download = 'myFile.txt';
+    document.body.appendChild(element);
+    element.click();
+  };
+
   return (
     <Popup {...{ open, handleToggle, title: 'Logs' }}>
       <div>{displayLogs}</div>
       <div className="popup__footer">
-        <Button className="popup__save">Save</Button>
+        <Button className="popup__save" onClick={HandleDownloadTextFile}>
+          Save
+        </Button>
       </div>
     </Popup>
   );
