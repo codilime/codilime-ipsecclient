846c53056eaa7b9bedddeaa6b300d477b337b8d9
Marcin Parafiniuk <marcin.parafiniuk@codilime.com>
2021-09-16 14:11:52 +0200

    monitoring fixes

--- ipsec-backend/frr.go
+++ ipsec-backend/frr.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func runTmpVtyshFile(tmpFile string) error {
@@ -21,16 +23,18 @@ func generateFRRTemplate(vrf Vrf) error {
 	if err := json.Unmarshal([]byte(vrf.Endpoints.String()), &endpoints); err != nil {
 		return err
 	}
-
-	createTmpFile := fmt.Sprintf("router bgp %d vrf %s\n  no bgp ebgp-requires-policy", vrf.LocalAs, vrf.ClientName)
+	fmt.Println("unmarshalled endpoints")
+	spew.Dump(endpoints)
+	createTmpFile := fmt.Sprintf("router bgp %d vrf %s\n  no bgp ebgp-requires-policy\n", vrf.LocalAs, vrf.ClientName)
 	for _, endpoint := range endpoints {
 		createTmpFile += fmt.Sprintf("  neighbor %s remote-as external\n", endpoint.PeerIP)
 	}
+	fmt.Println("generating frr template:")
+	fmt.Println(createTmpFile)
 	return runTmpVtyshFile(createTmpFile)
 }
 
 func deleteFRRTemplate(vrf Vrf) error {
-
 	deleteTmpFile := fmt.Sprintf("no router bgp %d vrf %s\n", vrf.LocalAs, vrf.ClientName)
 	return runTmpVtyshFile(deleteTmpFile)
 }
