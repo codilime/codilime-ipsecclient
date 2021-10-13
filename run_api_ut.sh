767847c6d8f8dfc34dc1bc3e4fafae154240e8df
Krzysztof Naglik <krzysztof.naglik@codilime.com>
2021-10-13 09:17:47 +0200

    Unit tests improvement

---
+++ run_api_ut.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+docker rm sico_api_ut || true
+
+docker run --name sico_api_ut \
+        --mount type=volume,source=ipsec,destination=/opt/ipsec/ \
+        --mount type=volume,source=frr,destination=/opt/frr/ \
+        --mount type=volume,source=super_net,destination=/opt/super_net/ \
+        --mount type=volume,source=super_api,destination=/opt/super_api/ \
+        sico_api_ut
