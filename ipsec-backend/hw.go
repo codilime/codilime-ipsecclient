21db85e46b6be67dc3d164af883b9e1f4ce79daf
Marcin Parafiniuk <marcin.parafiniuk@codilime.com>
2021-10-20 13:08:14 +0200

    ansible psk correctly sets up a hw tunnel

--- ipsec-backend/hw.go
+++ ipsec-backend/hw.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -142,32 +141,6 @@ func (a *App) restconfCreate(vrf Vrf) error {
 			return ReturnError(err)
 		}
 	}
-
-	// if err := a.restconfDoProposal(vrf, client, cryptoPh1); err != nil {
-	// 	return ReturnError(err)
-	// }
-	// if err := a.restconfDoPolicy(vrf, client); err != nil {
-	// 	return ReturnError(err)
-	// }
-	// if err := a.restconfDoEndpoints(vrf, client, vrf.Endpoints); err != nil {
-	// 	return ReturnError(err)
-	// }
-	// if err := a.restconfDoProfile(vrf, client); err != nil {
-	// 	return ReturnError(err)
-	// }
-	// if err := a.restconfDoTransformSet(vrf, cryptoPh2, client); err != nil {
-	// 	return ReturnError(err)
-	// }
-	// if err := a.restconfDoIpsecProfile(vrf, client, cryptoPh2[len(cryptoPh2)-1]); err != nil {
-	// 	return ReturnError(err)
-	// }
-	// if err := a.restconfDoTunnels(vrf, client, vrf.Endpoints); err != nil {
-	// 	return ReturnError(err)
-	// }
-	// if err := a.restconfDoBGP(vrf, client, vrf.Endpoints); err != nil {
-	// 	return ReturnError(err)
-	// }
-
 	return nil
 }
 
@@ -187,22 +160,22 @@ func (a *App) restconfDelete(vrf Vrf) error {
 			Error(err)
 		}
 	}
-	if err := a.tryRestconfDelete(fmt.Sprintf("Cisco-IOS-XE-native:native/crypto/ipsec/profile=%s", vrf.ClientName), client); err != nil {
+	if err := a.tryRestconfDelete("Cisco-IOS-XE-native:native/crypto/ipsec/profile=hardware_psk", client); err != nil {
 		Error(err)
 	}
-	if err := a.tryRestconfDelete(fmt.Sprintf("Cisco-IOS-XE-native:native/crypto/ipsec/transform-set=%s", vrf.ClientName), client); err != nil {
+	if err := a.tryRestconfDelete("Cisco-IOS-XE-native:native/crypto/ipsec/transform-set=hardware_psk", client); err != nil {
 		Error(err)
 	}
-	if err := a.tryRestconfDelete(fmt.Sprintf("Cisco-IOS-XE-native:native/crypto/ikev2/profile=%s", vrf.ClientName), client); err != nil {
+	if err := a.tryRestconfDelete("Cisco-IOS-XE-native:native/crypto/ikev2/profile=hardware_psk", client); err != nil {
 		Error(err)
 	}
-	if err := a.tryRestconfDelete(fmt.Sprintf("Cisco-IOS-XE-native:native/crypto/ikev2/keyring=%s", vrf.ClientName), client); err != nil {
+	if err := a.tryRestconfDelete("Cisco-IOS-XE-native:native/crypto/ikev2/keyring=hardware_psk", client); err != nil {
 		Error(err)
 	}
-	if err := a.tryRestconfDelete(fmt.Sprintf("Cisco-IOS-XE-native:native/crypto/ikev2/policy=%s", vrf.ClientName), client); err != nil {
+	if err := a.tryRestconfDelete("Cisco-IOS-XE-native:native/crypto/ikev2/policy=hardware_psk", client); err != nil {
 		Error(err)
 	}
-	if err := a.tryRestconfDelete(fmt.Sprintf("Cisco-IOS-XE-native:native/crypto/ikev2/proposal=%s", vrf.ClientName), client); err != nil {
+	if err := a.tryRestconfDelete("Cisco-IOS-XE-native:native/crypto/ikev2/proposal=hardware_psk", client); err != nil {
 		Error(err)
 	}
 	return nil
@@ -212,170 +185,6 @@ func (a *App) tryRestconfDelete(path string, client *http.Client) error {
 	return ReturnError(a.tryRestconfRequest("DELETE", path, "", client))
 }
 
-func (a *App) restconfDoProposal(vrf Vrf, client *http.Client, cryptoPh1 []string) error {
-	proposal := `{
-		"proposal": {
-		  "name": "%s",
-		  "encryption": {
-		    "%s": [null]
-		  },
-		  "integrity": {
-		    "%s": [null]
-		  },
-		  "prf": {
-		    "%s": [null]
-		  },
-		  "group": {
-		    "%s": [null]
-		  }
-		}
-	}`
-	proposalData := fmt.Sprintf(proposal, vrf.ClientName, cryptoPh1[0], cryptoPh1[1], cryptoPh1[1], cryptoPh1[2])
-	if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/crypto/ikev2/proposal", proposalData, client); err != nil {
-		return ReturnError(err)
-	}
-	return nil
-}
-
-func (a *App) restconfDoPolicy(vrf Vrf, client *http.Client) error {
-	policy := `{
-		"policy": {
-		  "name": "%s",
-		  "match": {
-			"fvrf": {
-			  "any": [null]
-			}
-		  },
-		  "proposal": {
-		    "proposals": "%s"
-		  }
-		}
-	}`
-	policyData := fmt.Sprintf(policy, vrf.ClientName, vrf.ClientName)
-	if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/crypto/ikev2/policy", policyData, client); err != nil {
-		return ReturnError(err)
-	}
-	return nil
-}
-
-func (a *App) restconfDoEndpoints(vrf Vrf, client *http.Client, dbEndpoints []Endpoint) error {
-
-	peers := make([]string, 0, len(dbEndpoints))
-	for i, Endpoint := range dbEndpoints {
-		if Endpoint.Authentication.Type == "certs" {
-			log.Debugf("skipping a cert endpoint")
-			continue
-		}
-		peer :=
-			`{
-			"name": "%s",
-			"address": {
-			  "ipv4": {
-			    "ipv4-address": "%s"
-			  }
-			},
-			"pre-shared-key": {
-			  "key": "%s"
-			}
-			}`
-		// TODO hw certs
-		peerData := fmt.Sprintf(peer, vrf.ClientName+strconv.Itoa(i), Endpoint.RemoteIPSec, Endpoint.Authentication.PSK)
-		peers = append(peers, peerData)
-	}
-
-	keyring := `{
-		"keyring": {
-		  "name": "%s",
-		  "peer": %s
-		}
-	}`
-	keyringData := fmt.Sprintf(keyring, vrf.ClientName, "["+strings.Join(peers, ",")+"]")
-	if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/crypto/ikev2/keyring", keyringData, client); err != nil {
-		return ReturnError(err)
-	}
-	return nil
-}
-
-func (a *App) restconfDoProfile(vrf Vrf, client *http.Client) error {
-	profile := `{
-		"profile": {
-		  "name": "%s",
-		  "authentication": {
-		    "local": {
-		      "pre-share": {}
-		    },
-		    "remote": {
-		      "pre-share": {}
-		    }
-		  },
-		  "keyring": {
-		    "local": {
-		      "name": "%s"
-		    }
-		  },
-		  "match": {
-		    "identity": {
-		      "remote": {
-			"any": [
-			  null
-			]
-		      }
-		    }
-		  }
-		}
-		}`
-	profileData := fmt.Sprintf(profile, vrf.ClientName, vrf.ClientName)
-	if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/crypto/ikev2/profile", profileData, client); err != nil {
-		return ReturnError(err)
-	}
-	return nil
-}
-
-func (a *App) restconfDoTransformSet(vrf Vrf, cryptoPh2 []string, client *http.Client) error {
-	// esp: always present
-	// key-bit: when it's gcm or aes
-	keyBit := ""
-	// esp-hmac: when it's not gcm
-	espHmac := ""
-	esp := ""
-	if containsADigit(cryptoPh2[0]) {
-		// this is gcm or aes
-		encFunc := strings.Split(cryptoPh2[0], "-")
-		if len(encFunc) < 3 {
-			return ReturnError(fmt.Errorf("wrong encryption function format: %s", cryptoPh2[0]))
-		}
-		keyBit = fmt.Sprintf(`"key-bit": "%s",`, encFunc[1])
-		esp = fmt.Sprintf("%s-%s", encFunc[0], encFunc[2])
-	} else {
-		esp = cryptoPh2[0]
-	}
-	if !strings.Contains(cryptoPh2[0], "gcm") {
-		espHmac = fmt.Sprintf(`"esp-hmac":"%s",`, cryptoPh2[1])
-	}
-	tunnelOptionName := "tunnel"
-	if os.Getenv("CAF_SYSTEM_NAME") == "cat9300X" {
-		tunnelOptionName = "tunnel-choice"
-	}
-	transformSet := `{
-		"transform-set": {
-		  "tag": "%s",
-		  "esp": "%s",
-		  %s
-		  %s
-		  "mode": {
-		    "%s": [
-		      null
-		    ]
-		  }
-		}
-		}`
-	transformSetData := fmt.Sprintf(transformSet, vrf.ClientName, esp, keyBit, espHmac, tunnelOptionName)
-	if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/crypto/ipsec/transform-set", transformSetData, client); err != nil {
-		return ReturnError(err)
-	}
-	return nil
-}
-
 func containsADigit(str string) bool {
 	for _, c := range str {
 		if c >= '0' && c <= '9' {
@@ -383,143 +192,6 @@ func containsADigit(str string) bool {
 		}
 	}
 	return false
-}
-
-func (a *App) restconfDoIpsecProfile(vrf Vrf, client *http.Client, cryptoName string) error {
-	ipsecProfile := `{
-		"profile": {
-		  "name": "%s",
-		  "set": {
-		    "ikev2-profile": "%s",
-		    "pfs": {
-		      "group": "%s"
-		    },
-		    "transform-set": [
-		      "%s"
-		    ],
-		    "security-association": {
-		      "lifetime": {
-			"kilobytes": "disable"
-		      }
-		    }
-		  }
-		}
-		}`
-	ipsecProfileData := fmt.Sprintf(ipsecProfile, vrf.ClientName, vrf.ClientName, cryptoName, vrf.ClientName)
-	if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/crypto/ipsec/profile", ipsecProfileData, client); err != nil {
-		return ReturnError(err)
-	}
-	return nil
-}
-
-func (a *App) restconfDoTunnels(vrf Vrf, client *http.Client, dbEndpoints []Endpoint) error {
-	for i, Endpoint := range dbEndpoints {
-		if Endpoint.Authentication.Type == "certs" {
-			log.Debugf("skipping a cert endpoint")
-			continue
-		}
-		tunnel := `{
-			"interface": {
-			  "Tunnel": {
-			    "name": %d,
-			    "description": "%s",
-			    "ip": {
-			      "address": {
-				"primary": {
-				  "address": "%s",
-				  "mask": "255.255.255.252"
-				}
-			      }
-			    },
-			    "Cisco-IOS-XE-tunnel:tunnel": {
-			      "source": "%s",
-			      "destination-config": {
-				"ipv4": "%s"
-			      },
-			      "mode": {
-				"ipsec": {
-				  "ipv4": {}
-				}
-			      },
-			      "protection": {
-				"Cisco-IOS-XE-crypto:ipsec": {
-				  "profile-option": {
-				    "name": "%s"
-				  }
-				}
-			      }
-			    }
-			  }
-			}
-			}`
-		tunnelData := fmt.Sprintf(tunnel, Endpoint.ID, vrf.ClientName+strconv.Itoa(i),
-			Endpoint.LocalIP, Endpoint.SourceInterface, Endpoint.RemoteIPSec, vrf.ClientName)
-		if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/interface", tunnelData, client); err != nil {
-			return ReturnError(err)
-		}
-	}
-	return nil
-}
-
-func (a *App) restconfDoBGP(vrf Vrf, client *http.Client, dbEndpoints []Endpoint) error {
-	bgp := `{
-		"bgp": [
-		  {
-		    "id": %d,
-		    "bgp": {
-		      "log-neighbor-changes": true
-		    },
-		    "neighbor": [
-		      %s
-		    ],
-		    "address-family":{
-			"no-vrf":{
-			   "ipv4":[
-			      {
-				 "af-name":"unicast",
-				 "ipv4-unicast":{
-				    "neighbor":[
-					    %s
-				    ],
-				    "redistribute":{
-				       "connected":{}
-				    }
-				 }
-			      }
-			   ]
-			}
-		     }
-		  }
-		]
-	      }`
-	neighbors := []string{}
-	neighbors2 := []string{}
-	neighbor := `{
-		"id": "%s",
-		"remote-as": %d
-	      }`
-	neighbor2 := `{
-		"id":"%s",
-		"activate":[
-		   null
-		]
-	     }`
-	for _, Endpoint := range dbEndpoints {
-		if Endpoint.Authentication.Type == "certs" {
-			log.Debugf("skipping a cert endpoint")
-			continue
-		}
-		if !Endpoint.BGP {
-			continue
-		}
-		neighbors = append(neighbors, fmt.Sprintf(neighbor, Endpoint.PeerIP, Endpoint.RemoteAS))
-		neighbors2 = append(neighbors2, fmt.Sprintf(neighbor2, Endpoint.PeerIP))
-	}
-	bgpData := fmt.Sprintf(bgp, vrf.LocalAs, strings.Join(neighbors, ","), strings.Join(neighbors2, ","))
-	if err := a.tryRestconfPatch("Cisco-IOS-XE-native:native/router/bgp", bgpData, client); err != nil {
-		return ReturnError(err)
-	}
-	return nil
 }
 
 func (a *App) tryRestconfPatch(path, data string, client *http.Client) error {
