12d9d7a6c8a9f37b0df46a9441cd8ebf3e38888d
jpod322 <jakub.podolski@codilime.com>
2023-02-10 13:40:16 +0100

    frr errors wrapped

--- ipsec-backend/config/frr.go
+++ ipsec-backend/config/frr.go
@@ -15,7 +15,6 @@ import (
 	"text/template"
 
 	"ipsec_backend/db"
-	"ipsec_backend/logger"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -23,11 +22,11 @@ import (
 func (f *SoftwareGenerator) storeFRRConfig(tmpFile string) error {
 	f.log.Info("storeFRRConfig invoked")
 	if err := f.FileHandler.WriteFile("/opt/frr/vtysh.conf", []byte(tmpFile), 0644); err != nil {
-		return logger.ReturnError(f.log, err)
+		return fmt.Errorf("write file: %w", err)
 	}
 
 	if err := f.Supervisor.ReloadVtysh(); err != nil {
-		return logger.ReturnError(f.log, err)
+		return fmt.Errorf("reload vtysh: %w", err)
 	}
 
 	log.Debugf("frr config stored:\n%s", tmpFile)
@@ -51,36 +50,46 @@ func (f *SoftwareGenerator) generateFRRConfig(vrf db.Vrf) error {
 	f.log.Info("generateFRRConfig invoked")
 	bytes, err := ioutil.ReadFile(templatesFolder + "frr.template")
 	if err != nil {
-		return logger.ReturnError(f.log, err)
+		return fmt.Errorf("read file: %w", err)
 	}
 
 	t, err := template.New("frr").Parse(string(bytes))
 	if err != nil {
-		return logger.ReturnError(f.log, err)
+		return fmt.Errorf("parse bytes: %w", err)
 	}
 
 	builder := strings.Builder{}
 	vlanList, err := vrf.GetVlans(f.log)
 	if err != nil {
-		return logger.ReturnError(f.log, err)
+		return fmt.Errorf("get vlands: %w", err)
 	}
 
 	if err = t.Execute(&builder, frrTemplateData{
 		Vrf:      vrf,
 		VlanList: vlanList,
 	}); err != nil {
-		return logger.ReturnError(f.log, err)
+		return fmt.Errorf("execute template: %w", err)
 	}
 
 	template := builder.String()
 	log.Debugf("generated frr template:\n%s", template)
 
-	return logger.ReturnError(f.log, f.storeFRRConfig(template))
+	err = f.storeFRRConfig(template)
+	if err != nil {
+		return fmt.Errorf("store frr config after generation: %w", err)
+	}
+	
+
+	return nil
 }
 
 func (f *SoftwareGenerator) deleteFRRConfig(vrf db.Vrf) error {
 	f.log.Info("deleteFRRConfig invoked")
 	frrConfig := fmt.Sprintf("no router bgp %d vrf %s\n", vrf.LocalAs, "vrf-"+strconv.Itoa(int(vrf.ID)))
 	f.log.Debug(frrConfig)
-	return logger.ReturnError(f.log, f.storeFRRConfig(frrConfig))
+	err := f.storeFRRConfig(frrConfig)
+	if err != nil {
+		return fmt.Errorf("store frr config during deletion: %w", err)
+	}
+	return nil
 }
